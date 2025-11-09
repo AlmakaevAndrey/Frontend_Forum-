670392e5a97a6f1f64332301c3d8df8dbefd1805
Andrey <almakaevandrey99@gmail.com>
2025-11-09 21:58:13 +0300

    fix client bug

--- server/dist/app.js
+++ server/dist/app.js
@@ -15,7 +15,7 @@ const path_1 = __importDefault(require("path"));
 dotenv_1.default.config();
 const app = (0, express_1.default)();
 app.use((0, cors_1.default)({
-    origin: process.env.CORS_ORIGIN || 'http://localhost:3000',
+    origin: process.env.CORS_ORIGIN || 'http://localhost:3000', //изменил CORS
     credentials: true,
     allowedHeaders: ['Content-Type', 'Authorization'],
 }));
