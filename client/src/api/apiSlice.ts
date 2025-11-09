75f40cb0327c94e02a6d29cd56480cc3811030c2
Andrey <almakaevandrey99@gmail.com>
2025-11-09 18:20:13 +0300

    Fix vercel deploy

--- client/src/api/apiSlice.ts
+++ client/src/api/apiSlice.ts
@@ -51,9 +51,11 @@ const axiosBaseQuery =
     }
   };
 
+const API_URL = process.env.REACT_APP_API_URL as string;
+
 export const ApiSlice = createApi({
   reducerPath: 'api',
-  baseQuery: axiosBaseQuery({ baseUrl: process.env.REACT_APP_API_URL }), // http://localhost:5000
+  baseQuery: axiosBaseQuery({ baseUrl: API_URL }), // http://localhost:5000
   tagTypes: ['Posts', 'Auth', 'Comments', 'User'],
   endpoints: (builder) => ({
     register: builder.mutation<
