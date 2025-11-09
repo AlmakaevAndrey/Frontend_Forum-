dc4a6c22eb9173bdb63fb54691d935f4c49cc731
Andrey <almakaevandrey99@gmail.com>
2025-11-09 20:33:11 +0300

    second fix deploy verser

--- client/jest.setup.ts
+++ client/jest.setup.ts
@@ -1,5 +1,7 @@
 import '@testing-library/jest-dom';
 const originalWarn = console.warn;
+(global as any).__API_URL__ = 'http://localhost:5000';
+
 beforeAll(() => {
   console.warn = (msg, ...args) => {
     if (
